9b65d46ff179c60d3bc25baeca2f85a3ba915cf1
MetalcoreLoli <deadpool1118@gmail.com>
2020-09-04 23:12:41 +0300

    fix regexies

--- thread2chDownloader.sh
+++ thread2chDownloader.sh
@@ -16,18 +16,21 @@ while [ -n "$1" ]; do
     shift
 done
 
-threadName=`echo "$link" | grep -o "(#[0-9]*)|(/[0-9]*)" | sed 's/^.//'`
+threadName=`echo "$link" | grep -o -E '([0-9]+.html)'| sed 's/\(\.html\)//g'`
 boardName=`echo $link | grep -o "/./res" | sed "s/\/res//"`
-fileRegex="$boardName/src/$threadName\S*"
-filesFromThread=$(curl $link | grep -o "$fileRegex" | sed 's/"//' | sed 's/>//' )
+fileRegex="$boardName/src/$threadName/\S*"
+filesFromThread=$(curl $link | grep -o -E "$fileRegex" | sed 's/\("\|>\)//g'| sort -u)
 
-unique=$(echo "${filesFromThread[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
-printf "Begin\n"
-for str in $unique; do
+
+begin=$(date | grep -o -E '([0-9]{2}:[0-9]{2}:[0-9]{2})+|([0-9]:[0-9]:[0-9])+')
+
+printf "Begin at $begin\n"
+for str in $filesFromThread; do
     fullFileName="https://2ch.hk$str"
     curl -s -O "$fullFileName" > /dev/null 
-    time=`date | awk '{print $4}'`
+    time=$(date | grep -o -E '([0-9]{2}:[0-9]{2}:[0-9]{2})+|([0-9]:[0-9]:[0-9])+')
     printf "[$fullFileName] was \033[38;2;0;156;50mdownloaded\033[39m at $time\n"
 done
-printf "End\n"
+end=$(date | grep -o -E '([0-9]{2}:[0-9]{2}:[0-9]{2})+|([0-9]:[0-9]:[0-9])+')
+printf "End at $end\n"

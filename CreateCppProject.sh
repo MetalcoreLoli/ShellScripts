72bdc8702e19d11277552c81d842989f54f76879
my name <deadpool1118@gmail.com>
2021-07-28 16:03:19 +0300

    refactor und rename CreateCppProject.sh to new-cpp

--- CreateCppProject.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir $1 && cd $1
-mkdir sources && mkdir headers && mkdir bin
-
-printf "build:\n\tg++ -g main.cpp -o bin/app\nrun:\n\t./bin/app" >> Makefile 
-printf "#include <iostream>\n\nint main()\n{\n\tstd::cout << \"Hello World!\" << std::endl;;\n\treturn 0;\n}" >> main.cpp 
-
-make build && make run
